5e1a262a6ff1aff67f3f9dfa418bdde5c9659cf2
ethancs777 <ethancs777@gmail.com>
2025-09-02 17:23:07 -0700

    Added basic landing page and setup files for development of more pages when they are needed.

--- todoproject/todo/urls.py
+++ todoproject/todo/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path("manage/", views.task_management, name='task_management'),
+    path("tasks/", views.view_tasks, name='view_tasks'),
 ]
--- todoproject/todo/views.py
+++ todoproject/todo/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, this is the home page of the To-Do app.")
+    return render(request, 'todo/home.html')
+
+def task_management(request):
+    return render(request, 'todo/task_management.html')
+
+def view_tasks(request):
+    return render(request, 'todo/view_tasks.html')
